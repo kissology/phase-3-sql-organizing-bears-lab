8ca1e6ed7b98d1fa9b29e32603d3c47ac84d0c11
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2022-12-15 19:41:46 -0500

    commit tree f1e3da5d6eb0a2ebf00525283eb51126466f76e6

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+ (1,'Mr. Chocolate', 25, 'M', 'Brown', 'Happy', 0)
+ (2,'Rowdy', 24, 'F', 'Blue', 'Grumpy', 1)
+ (3,'Tabitha', 23, 'F', 'Black', 'Sad', 1)
+ (4,'Sergeant Brown', 65, 'White', 'Jolly', 1)
+ (5,'Melissa', 45, 'Red', 'Sassy', 1)
+ (6,'Grinch', 30, 'Green', 'Shady', 0)
+ (7,'Wendy',  45, 'Yellow', 'Sunny', 1)
+ (8,'NULL', 45, 'White', 'Apathetic', 1)
+
